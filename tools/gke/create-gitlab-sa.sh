898ab35445148427fcfbeac10b8b278b89606587
Pierre-Yves Aillet <pyaillet@gmail.com>
2019-12-12 22:33:45 +0100

    :bug: fix bugs

--- tools/gke/create-gitlab-sa.sh
+++ tools/gke/create-gitlab-sa.sh
@@ -2,5 +2,9 @@
 
 kubectl create ns gitlab
 kubectl create -n gitlab serviceaccount gitlab
-kubectl create -n gitlab clusterrolebinding gitlab-cluster-admin-binding --clusterrole=cluster-admin --service-accound=gitlab:gitlab
-kubectl get -n gitlab $(kubectl get -n gitlab secret -o name | grep gitlab) -o yaml
+kubectl create -n gitlab clusterrolebinding gitlab-cluster-admin-binding --clusterrole=cluster-admin --serviceaccount=gitlab:gitlab
+kubectl get -n gitlab $(kubectl get -n gitlab secret -o name | grep gitlab) -o json
+
+echo "API URL: $(gcloud container clusters describe nc-kubedev --format json | jq -r '.endpoint')"
+echo "CA Certificate:\n$(gcloud container clusters describe nc-kubedev --format json | jq -r '.masterAuth.clusterCaCertificate' | base64 -D)"
+echo "Service Token: $(kubectl get -n gitlab $(kubectl get -n gitlab secret -o name | grep gitlab) -o json | jq -r '.data.token' | base64 -D)"
